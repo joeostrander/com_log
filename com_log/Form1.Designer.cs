1dc386af6b99357cf8277efe4d7f906bc575faa0
Joe Ostrander <ostrander.jm@gmail.com>
2020-04-01 09:16:19 -0500

    Add project files.

---
+++ com_log/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace com_log
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.comboBoxComPort1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.labelReceived = new System.Windows.Forms.Label();
+            this.labelBytesReceivedValue = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabelFilename = new System.Windows.Forms.LinkLabel();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.checkBoxAppend = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(334, 33);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 19;
+            this.buttonConnect.Text = "&Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // comboBoxComPort1
+            // 
+            this.comboBoxComPort1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxComPort1.FormattingEnabled = true;
+            this.comboBoxComPort1.Location = new System.Drawing.Point(16, 35);
+            this.comboBoxComPort1.Name = "comboBoxComPort1";
+            this.comboBoxComPort1.Size = new System.Drawing.Size(312, 21);
+            this.comboBoxComPort1.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "COM Port:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 167);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(425, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 21;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // labelReceived
+            // 
+            this.labelReceived.AutoSize = true;
+            this.labelReceived.Location = new System.Drawing.Point(16, 128);
+            this.labelReceived.Name = "labelReceived";
+            this.labelReceived.Size = new System.Drawing.Size(80, 13);
+            this.labelReceived.TabIndex = 22;
+            this.labelReceived.Text = "Bytes received:";
+            // 
+            // labelBytesReceivedValue
+            // 
+            this.labelBytesReceivedValue.AutoSize = true;
+            this.labelBytesReceivedValue.Location = new System.Drawing.Point(102, 128);
+            this.labelBytesReceivedValue.Name = "labelBytesReceivedValue";
+            this.labelBytesReceivedValue.Size = new System.Drawing.Size(13, 13);
+            this.labelBytesReceivedValue.TabIndex = 23;
+            this.labelBytesReceivedValue.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "&Filename:";
+            // 
+            // linkLabelFilename
+            // 
+            this.linkLabelFilename.AutoSize = true;
+            this.linkLabelFilename.Location = new System.Drawing.Point(102, 73);
+            this.linkLabelFilename.Name = "linkLabelFilename";
+            this.linkLabelFilename.Size = new System.Drawing.Size(58, 13);
+            this.linkLabelFilename.TabIndex = 27;
+            this.linkLabelFilename.TabStop = true;
+            this.linkLabelFilename.Text = "click to set";
+            this.linkLabelFilename.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelFilename_LinkClicked);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(265, 98);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(144, 42);
+            this.buttonStart.TabIndex = 28;
+            this.buttonStart.Text = "&Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // checkBoxAppend
+            // 
+            this.checkBoxAppend.AutoSize = true;
+            this.checkBoxAppend.Location = new System.Drawing.Point(105, 98);
+            this.checkBoxAppend.Name = "checkBoxAppend";
+            this.checkBoxAppend.Size = new System.Drawing.Size(40, 17);
+            this.checkBoxAppend.TabIndex = 29;
+            this.checkBoxAppend.Text = "No";
+            this.checkBoxAppend.UseVisualStyleBackColor = true;
+            this.checkBoxAppend.CheckedChanged += new System.EventHandler(this.checkBoxAppend_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Append?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 189);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBoxAppend);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.linkLabelFilename);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelBytesReceivedValue);
+            this.Controls.Add(this.labelReceived);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBoxComPort1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.ComboBox comboBoxComPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label labelReceived;
+        private System.Windows.Forms.Label labelBytesReceivedValue;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabelFilename;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.CheckBox checkBoxAppend;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ com_log/Form1.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace com_log
+{
+    public partial class Form1 : Form
+    {
+        private static SerialPort serialPort1 = new SerialPort();
+
+        private static List<KeyValuePair<string, string>> listPorts1 = new List<KeyValuePair<string, string>>();
+        private static bool boolConnected;
+
+        private static FileStream filestream_log;
+        //private static MemoryStream mStream;
+        //private int totalBytes;
+        private string logFilename;
+        private bool bool_logging = false;
+
+        public void ThreadSafeDelegate(MethodInvoker method)
+        {
+            if (InvokeRequired)
+                BeginInvoke(method);
+            else
+                method.Invoke();
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            //if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    try
+            //    {
+            //        //string filename = saveFileDialog1.FileName;
+            //        //FileStream file = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            //        mStream.Seek(0, SeekOrigin.Begin);
+            //        mStream.WriteTo(filestream_log);
+            //        filestream_log.Close();
+            //        mStream.Seek(0, SeekOrigin.End);    // move to end in case we want to append...
+
+            //        MessageBox.Show("See file:  " + logFilename, "File Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //    }
+            //}
+
+            close_log_file(false);
+            //buttonStart.Enabled = true;
+            //buttonStop.Enabled = false;
+            //buttonStart.Text = "&Start";
+
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //textBoxReceived.Font = new Font(FontFamily.GenericMonospace, textBoxReceived.Font.Size);
+
+            LoadPortsList();
+
+            comboBoxComPort1.DataSource = listPorts1;
+            comboBoxComPort1.ValueMember = "Key";
+            comboBoxComPort1.DisplayMember = "Value";
+
+
+            this.Text = Application.ProductName;
+
+            timer1.Enabled = true;
+        }
+
+
+        private void LoadPortsList()
+        {
+            listPorts1.Clear();
+            string strQuery = "Select * from Win32_PnPEntity Where Name LIKE '% (COM%)'";
+
+            listPorts1.Add(new KeyValuePair<string, string>("", ""));
+
+            try
+            {
+
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(strQuery);
+                ManagementObjectCollection collection = searcher.Get();
+
+                foreach (ManagementObject item in collection)
+                {
+                    string fn = item.Properties["Name"].Value.ToString();
+                    string strPattern = @"^(?<porttext>.*?)\((?<portname>COM[\d]+)\)$";
+                    Match m = Regex.Match(fn, strPattern);
+
+                    if (m.Success)
+                    {
+                        string portname = m.Groups["portname"].Value.ToString();
+                        string porttext = m.Groups["porttext"].Value.ToString();
+                        listPorts1.Add(new KeyValuePair<string, string>(portname, portname.PadRight(6, ' ') + porttext));
+                    }
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Could not get serial port list!", "Serial Ports", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+
+
+
+            buttonConnect.Enabled = false;
+            if (boolConnected)
+            {
+                disconnect();
+            }
+            else
+            {
+                connect();
+            }
+        }
+
+        private void choose_log_file()
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                logFilename = saveFileDialog1.FileName;
+                linkLabelFilename.Text = logFilename;
+            }
+        }
+
+        private void disconnect()
+        {
+            boolConnected = false;
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+
+            buttonConnect.Enabled = true;
+
+            bool_logging = false;
+
+            // maybe not... close_log_file(true);
+
+        }
+
+        private void close_log_file(bool silent)
+        {
+            try
+            {
+                if (filestream_log != null)
+                {
+                    filestream_log.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (silent)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                else
+                {
+                    MessageBox.Show("Error closing log file.\n\n" + ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+
+        }
+
+        private void connect()
+        {
+            string strPortName1 = comboBoxComPort1.SelectedValue.ToString();
+            if (string.IsNullOrEmpty(strPortName1))
+            {
+                buttonConnect.Enabled = true;
+                return;
+            }
+            //serialPort1 = new SerialPort(strPortName, 9600, Parity.None, 8, StopBits.One);
+            serialPort1.PortName = strPortName1;
+            serialPort1.BaudRate = 115200;
+            serialPort1.ReadBufferSize = 1024;
+            //serialPort1.Encoding = Encoding.UTF8;
+
+            serialPort1.Parity = Parity.None;
+            //serialPort1.Handshake = Handshake.XOnXOff;  // TODO... should I do this? does GCA term do it?
+            serialPort1.Handshake = Handshake.None;
+            serialPort1.DataBits = 8;
+            serialPort1.StopBits = StopBits.One;
+            //serialPort1.WriteBufferSize = 1;
+
+
+            try
+            {
+                serialPort1.Open();
+                boolConnected = true;
+                //mStream = new MemoryStream();
+
+                //totalBytes = 0;
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                boolConnected = false;
+
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+            }
+
+            buttonConnect.Enabled = true;
+            //clearBytes();
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+
+                SerialPort sp = (SerialPort)sender;
+
+                int byteLength = sp.BytesToRead;
+                byte[] buffer = new byte[byteLength];
+                sp.Read(buffer, 0, byteLength);
+
+
+                foreach (byte b in buffer)
+                {
+                    Console.Write(b.ToString("X2") + " ");
+                    
+                    // TODO append to file
+                    //mStream.WriteByte(unescaped);
+                    //mStream.WriteByte(b);
+                    if (filestream_log != null)
+                    {
+                        filestream_log.WriteByte(b);
+                    }
+                }
+
+                //ThreadSafeDelegate(delegate {
+                //    labelBytesReceivedValue.Text = mStream.Length.ToString();
+                //});
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+
+        private void enableControls(bool enable)
+        {
+
+            try
+            {
+                foreach (Control item in this.Controls)
+                {
+                    if (item.GetType().ToString() == "System.Windows.Forms.ComboBox")
+                    {
+                        item.Enabled = enable;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //do nothing
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (!string.IsNullOrEmpty(serialPort1.PortName))
+                {
+                    if (serialPort1.IsOpen)
+                    {
+                        boolConnected = true;
+                        comboBoxComPort1.Enabled = false;
+                    }
+                    else
+                    {
+                        boolConnected = false;
+                        comboBoxComPort1.Enabled = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                boolConnected = false;
+                comboBoxComPort1.Enabled = true;
+                Console.WriteLine(ex.Message);
+            }
+
+
+            if (boolConnected)
+            {
+                buttonConnect.Text = "&Disconnect";
+                toolStripStatusLabel1.Text = "Connected to port:  " + serialPort1.PortName;
+
+            }
+            else
+            {
+                buttonConnect.Text = "&Connect";
+                toolStripStatusLabel1.Text = "Disconnected";
+            }
+
+            buttonStart.Text = bool_logging ? "&Stop" : "&Start";
+
+            enableControls(!boolConnected);
+
+            //if (mStream != null)
+            //{
+            //    labelBytesReceivedValue.Text = mStream.Length.ToString();
+            //}
+
+            if (filestream_log != null)
+            {
+                if (filestream_log.CanWrite)
+                    labelBytesReceivedValue.Text = filestream_log.Length.ToString();
+            }
+
+        }
+
+        //private void buttonClear_Click(object sender, EventArgs e)
+        //{
+        //    if (mStream != null)
+        //    {
+        //        if (mStream.Length > 0)
+        //        {
+        //            if (MessageBox.Show("Clear existing data?",Application.ProductName,MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+        //            {
+        //                mStream = new MemoryStream();
+        //            }
+        //        }
+        //    }
+        //}
+
+        private void linkLabelFilename_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            choose_log_file();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(logFilename))
+            {
+                choose_log_file();
+
+                if (string.IsNullOrEmpty(logFilename))
+                    return;
+            }
+
+            if (!boolConnected)
+            {
+                return;
+            }
+
+            if (bool_logging)
+            {
+                stop_logging();
+            }
+            else
+            {
+                start_logging();
+            }
+
+        }
+
+        private void stop_logging()
+        {
+            try
+            {
+                filestream_log.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+                
+            //buttonStart.Text = "&Start";
+            bool_logging = false;
+        }
+
+        private void start_logging()
+        {
+
+            try
+            {
+                FileMode filemode = checkBoxAppend.Checked ? FileMode.Append : FileMode.Create;
+                filestream_log = new FileStream(logFilename, filemode, FileAccess.Write);
+
+                //buttonStart.Enabled = false;
+                //buttonStop.Enabled = true;
+                buttonStart.Text = "&Stop";
+                bool_logging = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void checkBoxAppend_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBoxAppend.Text = checkBoxAppend.Checked ? "Yes" : "No";
+        }
+    }
+}
